4563739125529caa396a631b9ebc7efaac5cbe05
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-04-25 19:20:04 +0200

    add project but there are still issues to be handled:submit doesnt work and seonds to mins must be converted

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -10,3 +10,32 @@
   If the timer finishes the timer should be replaced by the message: Time 's up!
  * 
  */
+
+let s = 5;//document.querySelector('.form-example');
+let m = 1; //document.querySelector('#min');
+let timer;
+const stopWatch = document.querySelector('.stopwatch');
+
+function start() {
+  if (!timer){
+  timer = setInterval(run, 1000);
+  }
+}
+
+function run() {
+  m = Math.floor(s / 60);
+  stopWatch.textContent = m + ':' + s;
+  s--;
+  if(s < 0 && m < 0) {
+    clearInterval(timer);
+    stopWatch.textContent = "Time\'s up!"
+  }
+
+} 
+
+function pause() {
+  clearInterval(timer);
+  timer = false;
+}
+
+
