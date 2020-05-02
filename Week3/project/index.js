438c80692770752372b00d915587f075adb07ab0
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-05-02 17:48:59 +0200

    modify js alert, css people question input center

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,27 @@
 // Your code goes in here
+const costInput = document.querySelector('#cost');
+const serviceInput = document.querySelector('.service');
+const peopleInput = document.querySelector('#people');
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
 
-document.querySelector("#app").innerText = "Tip Calculator";
+function calculateTip () { //inspired by https://www.youtube.com/watch?v=eJIHT0cENR0
+ let cost = parseInt(costInput.value);
+ let service = parseInt(serviceInput.value) / 100;
+ let people = parseInt(peopleInput.value);
+ let tip = (cost * service) / people;
+ 
+ if(!cost || !service || !people ){
+         return alert('Invalid number!')
+     }
+//  else if(!cost || !service || !people){
+//     return alert('Invalid number!')
+
+//  else if(cost || service || people ===  null){
+//      return alert('Please fill in all the fields!')
+//  }
+if (people > 1) {
+    return output.textContent = '€' + tip.toFixed(2) + ' each';
+}
+return  output.textContent = '€' + tip.toFixed(2) ;
+}
